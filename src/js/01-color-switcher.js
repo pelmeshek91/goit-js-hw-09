afb203c030119fa9643717bdededc0f299ed2c65
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-05-02 16:36:44 +0100

    complete task -1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let intevalId = null;
 
+function changeColor() {
+  intevalId = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+
+    btnStart.setAttribute('disabled', true);
+    btnStop.removeAttribute('disabled');
+  }, 1000);
+}
+
+function stopChangeColor() {
+  clearInterval(intevalId);
+
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+btnStart.addEventListener('click', changeColor);
+btnStop.addEventListener('click', stopChangeColor);
